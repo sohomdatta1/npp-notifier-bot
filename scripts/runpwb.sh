21a157b8784fc8d2e93065aeca2b1eabbee818cb
Sohom Datta <sdatta4@ncsu.edu>
2025-01-07 10:29:24 -0500

    Remove broken logger

--- scripts/runpwb.sh
+++ scripts/runpwb.sh
@@ -2,4 +2,4 @@
 id
 cat user-config.tmpl > user-config.py
 ls -lah user-config.py
-python3 $@ > $TOOL_DATA_DIR/logs/notifier_$(date + "%F %T").log
+python3 $@ #> $TOOL_DATA_DIR/logs/notifier_$(date + "%F %T").log
